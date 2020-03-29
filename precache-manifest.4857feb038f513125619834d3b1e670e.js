ddbbc007338b3d12fd06588e7593fa7154f23b0b
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-03-29 19:46:30 -0300

    Updates

---
+++ precache-manifest.4857feb038f513125619834d3b1e670e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca4efe7cccd359d4576e7e37c116a061",
+    "url": "/hunting-words/index.html"
+  },
+  {
+    "revision": "6ffbaf9c285e75c3ab58",
+    "url": "/hunting-words/static/css/main.05a223fa.chunk.css"
+  },
+  {
+    "revision": "75de20cd51bacdf3efd9",
+    "url": "/hunting-words/static/js/2.87ab8b08.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hunting-words/static/js/2.87ab8b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffbaf9c285e75c3ab58",
+    "url": "/hunting-words/static/js/main.01e5c919.chunk.js"
+  },
+  {
+    "revision": "8529a4f0b619976d0c07",
+    "url": "/hunting-words/static/js/runtime-main.efb7a0dc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d9601e218e16b533d1730895fdda35c0.js"
+  "/hunting-words/precache-manifest.4857feb038f513125619834d3b1e670e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hunting-words/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
