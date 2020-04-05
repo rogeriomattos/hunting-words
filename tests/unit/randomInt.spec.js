324df17262583da0f459a51c469d7509d485e26e
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-04-05 19:52:45 -0300

    publish new version

--- tests/unit/randomInt.spec.js
+++ tests/unit/randomInt.spec.js
@@ -2,22 +2,36 @@ const randomInt = require('../../src/randomInt');
 describe('Random Int', () => {
 
     it('returns should a number type', () => {
-        const returns = randomInt(0, 10);
+        const randomNumber = randomInt(0, 10);
 
-        expect(typeof returns).toBe('number');
+        expect(typeof randomNumber).toBe('number');
     });
 
-    it('should generate a number betwen 0 and 10', () => {
-        const returns = randomInt(0, 10);
+    it('should generate a number between 0 and 10', () => {
+        const randomNumber = randomInt(0, 10);
 
-        expect(returns).toBeGreaterThanOrEqual(0);
-        expect(returns).toBeLessThanOrEqual(10);
+        expect(randomNumber).toBeGreaterThanOrEqual(0);
+        expect(randomNumber).toBeLessThanOrEqual(10);
     });
 
-    it('should generate a number betwen 4 and 5', () => {
-        const returns = randomInt(4, 5);
+    it('should generate a number between 4 and 5', () => {
+        const randomNumber = randomInt(4, 5);
 
-        expect(returns).toBeGreaterThanOrEqual(4);
-        expect(returns).toBeLessThanOrEqual(5);
-    }); 
+        expect(randomNumber).toBeGreaterThanOrEqual(4);
+        expect(randomNumber).toBeLessThanOrEqual(5);
+    });
+    
+    it('should generate a number between 0 and 1', () => {
+        const randomNumber = randomInt(0, 1);
+
+        expect(randomNumber).toBeGreaterThanOrEqual(0);
+        expect(randomNumber).toBeLessThanOrEqual(1);
+    });
+
+    it('should generate a number between -3 and -1', () => {
+        const randomNumber = randomInt(-3, -1);
+
+        expect(randomNumber).toBeGreaterThanOrEqual(-3);
+        expect(randomNumber).toBeLessThanOrEqual(-1);
+    });
 });
