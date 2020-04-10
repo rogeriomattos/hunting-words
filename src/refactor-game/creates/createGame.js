3b3391ea895ec642afe25b7323da561f85de8b1d
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-04-10 13:56:36 -0300

    criando options do game

--- src/refactor-game/creates/createGame.js
+++ src/refactor-game/creates/createGame.js
@@ -1,17 +1,34 @@
 const createBoard = require('./createBoard');
 
-function createGame(rows, columns, words){
+
+/**
+ * 
+ * @param {number} rows 
+ * @param {number} columns 
+ * @param {Array<string>} words 
+ * @param {object{wordsCross:boolen, inverseWord:boolean, wordInVertical:boolean, wordInHorizontal:boolean, wordDiagonalLeft: boolen, wordDiagonalRight: boolen}} options 
+ */
+function createGame(rows, columns, words, options){
+    options = {
+        wordsCross: (!options || options.wordsCross != false), 
+        inverseWord: (!options || options.inverseWord != false), 
+        wordInVertical: (!options || options.wordInVertical != false), 
+        wordInHorizontal: (!options || options.wordInHorizontal != false),  
+        wordDiagonalLeft: (!options || options.wordDiagonalLeft != false),  
+        wordDiagonalRight: (!options || options.wordDiagonalRight != false) 
+    };
+
     this.board = new createBoard(rows, columns, words);
     this.words = words;
     this.rows = rows;
     this.columns = columns;
 }
 
-let game = new createGame(30, 40, ["teste", "Rogerio", "Palavra"]);
+let game = new createGame(30, 40, ["teste", "Rogerio", "Palavra"], {});
 
-console.log(game);
+//console.log(game);
 
-console.log(game.board[0][8]);
+//console.log(game.board[0][8]);
 
 module.exports = createGame;
 
